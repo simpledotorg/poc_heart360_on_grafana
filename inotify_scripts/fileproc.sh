aeb8a38e9315bf8deea143b69de1d2159b7ac217
Igbanam <390059+igbanam@users.noreply.github.com>
2025-12-09 13:16:47 +0000

    replace dockerpinata/inotify-tools with devodev/inotify (#1)
    * replace dockerpinata/inotify-tools with devodev/inotify
    
    * cosmetics

--- inotify_scripts/fileproc.sh
+++ inotify_scripts/fileproc.sh
@@ -1,4 +1,21 @@
-[ -d "$1" ] || [ ! -f "$1" ] && exit 0
+#!/bin/bash
 
+# $1 --> timestamp
+# $2 --> watched file/directory path
+# $3 --> event name(s)
+# $4 --> filename (if a directory is monitored)
 
-echo "We got a file !! $1"
+echo $1 $2 $3 $4
+
+FULL_PATH="${2}${4}"
+
+if [ ! -f "$FULL_PATH" ]; then
+    echo "Info: '$FULL_PATH' is not a regular file or does not exist. Exiting."
+    exit 0
+fi
+
+echo "We got a file !! $FULL_PATH"
+
+# if [[ "$3" == "MODIFY" ]]; then
+#   INPUT_FILE=$FULL_PATH python prepare_date.py
+# fi
