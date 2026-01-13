7d1fffec2bf6e429e33f9fc7a3c18c77679c67b9
Arnaud <ademarcq@rtsl.org>
2026-01-13 13:05:25 +0100

    Fixing typo

--- pg_init_scripts/00_schemas.sql
+++ pg_init_scripts/00_schemas.sql
@@ -1,3 +1,8 @@
 CREATE USER grafana WITH PASSWORD 'your_secure_password';
 CREATE SCHEMA grafana_schema AUTHORIZATION grafana;
 ALTER ROLE grafana SET search_path TO grafana_schema, public;
+
+
+CREATE USER heart360tk WITH PASSWORD 'your_secure_password';
+CREATE SCHEMA heart360tk AUTHORIZATION heart360tk;
+ALTER ROLE heart360tk SET search_path TO heart360tk, public;
