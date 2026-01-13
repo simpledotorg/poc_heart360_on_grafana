248f5b5414665834bb6aafbb5fbfd563b3d8d003
Arnaud <ademarcq@rtsl.org>
2026-01-13 15:52:43 +0100

    introduces fileproc image to dockerhub

--- .github/workflows/dockerpush.sh
+++ .github/workflows/dockerpush.sh
@@ -6,17 +6,22 @@
 ###
 docker push ${DOCKER_REPO}/heart360tk-grafana:latest
 docker push ${DOCKER_REPO}/heart360tk-postgresql:latest
+docker push ${DOCKER_REPO}/heart360tk-fileprocessor:latest
 
 docker push ${DOCKER_REPO}/heart360tk-grafana:${BRANCH_NAME}
 docker push ${DOCKER_REPO}/heart360tk-postgresql:${BRANCH_NAME}
+docker push ${DOCKER_REPO}/heart360tk-fileprocessor:${BRANCH_NAME}
 
 docker push ${DOCKER_REPO}/heart360tk-grafana:${BRANCH_NAME}.${BUILD_NUMBER}
 docker push ${DOCKER_REPO}/heart360tk-postgresql:${BRANCH_NAME}.${BUILD_NUMBER}
+docker push ${DOCKER_REPO}/heart360tk-fileprocessor:${BRANCH_NAME}.${BUILD_NUMBER}
 
 
 docker push ${DOCKER_REPO}/heart360tk-grafana:${BRANCH_NAME}.${COMMIT_HASH}
 docker push ${DOCKER_REPO}/heart360tk-postgresql:${BRANCH_NAME}.${COMMIT_HASH}
+docker push ${DOCKER_REPO}/heart360tk-fileprocessor:${BRANCH_NAME}.${COMMIT_HASH}
 
 docker push ${DOCKER_REPO}/heart360tk-grafana:${COMMIT_HASH}
 docker push ${DOCKER_REPO}/heart360tk-postgresql:${COMMIT_HASH}
+docker push ${DOCKER_REPO}/heart360tk-fileprocessor:${COMMIT_HASH}
 
